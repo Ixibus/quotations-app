22058175041e2da569e233190760c157dad6c21e
Franck CORCHER <corcher.franck@gmail.com>
2025-11-17 12:05:05 +0100

    count_manager_js

--- script.js
+++ script.js
@@ -1,40 +1,53 @@
+// Déclaration de variables globales
+
 const body = document.querySelector("body");
 const citationInput = document.getElementById("citation_input");
 const authorInput = document.getElementById("author_input");
 const submit = document.getElementById("submitAction");
 const form = document.querySelector("form");
 
+const count = document.querySelector("h3");
+
+// ----- Compteur de citations -----
+
+let quoteCount = 0;
+
+function counter() {
+  quoteCount++;
+
+  quoteCount === 1 ? (count.textContent = "1 citation") : (count.textContent = `${quoteCount} citations`);
+}
+
+// ----- Ajouteur de citation -----
+
 function addQuote(quote, author) {
+  // 1. Création de nouvelle citation avec l'auteur
+  const textP = document.createElement("p");
+  const innerText = document.createTextNode(quote);
+  textP.setAttribute("class", "text");
+  textP.appendChild(innerText);
+  const authorP = document.createElement("p");
+  const innerAuthorName = document.createTextNode(author);
+  authorP.setAttribute("class", "author");
+  authorP.appendChild(innerAuthorName);
 
-    // 1. Création de nouvelle citation avec l'auteur
-    const textP = document.createElement("p");
-    const innerText = document.createTextNode(quote);
-    textP.setAttribute("class", "text");
-    textP.appendChild(innerText);
-    const authorP = document.createElement("p");
-    const innerAuthorName = document.createTextNode(author);
-    authorP.setAttribute("class", "author");
-    authorP.appendChild(innerAuthorName);
-    
-    // 2. Insertion de la nouvelle création dans un container 
-    const quoteContainer = document.createElement("div");
-    quoteContainer.appendChild(textP);
-    quoteContainer.appendChild(authorP);
-    
-    // 3. Insertion de ce nouveau container dans le dom dans dans "quote-list" 
-    const quoteListContainer = document.getElementById("quote-list");
-    quoteListContainer.appendChild(quoteContainer);
+  // 2. Insertion de la nouvelle création dans un container
+  const quoteContainer = document.createElement("div");
+  quoteContainer.appendChild(textP);
+  quoteContainer.appendChild(authorP);
+
+  // 3. Insertion de ce nouveau container dans le dom dans dans "quote-list"
+  const quoteListContainer = document.getElementById("quote-list");
+  quoteListContainer.appendChild(quoteContainer);
 }
 
-console.log(citationInput.value)
-console.log(authorInput.value)
+// Evenement au clique
+submit.addEventListener("click", () => {
+  addQuote(citationInput.value, authorInput.value);
+  counter();
+});
 
-
-submit.addEventListener('click', () => { 
-    addQuote(citationInput.value,authorInput.value);
-}
-)
-
-form.addEventListener('click', (e) => {
-e.preventDefault();
-})
+// Eviter le chargement autommatique du "form"
+form.addEventListener("click", (e) => {
+  e.preventDefault();
+});
